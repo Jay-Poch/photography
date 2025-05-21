7f2224d8a6ae804d4a7ca88994994e2ed8f24064
Jaypoch <technobladedeutsch@gmail.com>
2025-05-21 19:13:00 +0200

    beutify it

--- frontend/script.js
+++ frontend/script.js
@@ -14,18 +14,18 @@ const contentId = urlParams.get('content_id');
 console.log(contentId);
 
 async function run() {
-    let result = await myFunction(contentId);
-    //console.log(result)
-    document.getElementById('image').src = result.location;
-    document.getElementById('textbox2').textContent = 
+  let result = await myFunction(contentId);
+  //console.log(result)
+  document.getElementById('image').src = result.location;
+  document.getElementById('textbox2').textContent =
     "Kameramodell: " + result.info.ISO
-+ "\nVerwendetes Objektiv: " + result.info.camera_model 
-+ "\nBelichtungszeit: "  + result.info.lens_used
-+ "\nBlende: "  + result.info.shutter_speed
-+ "\nISO: "  + result.info.aperture
-+ "\nBeleuchtung: "  + result.info.lighting
-+ "\nFokuspunkt: "  + result.info.focal_point
-+ "\nFotogenre: "  + result.info.photo_genre
+    + "\nVerwendetes Objektiv: " + result.info.camera_model
+    + "\nBelichtungszeit: " + result.info.lens_used
+    + "\nBlende: " + result.info.shutter_speed
+    + "\nISO: " + result.info.aperture
+    + "\nBeleuchtung: " + result.info.lighting
+    + "\nFokuspunkt: " + result.info.focal_point
+    + "\nFotogenre: " + result.info.photo_genre
 }
 run();
 function info(text) {
@@ -33,17 +33,17 @@ function info(text) {
   document.getElementById('toast').classList.remove('hidden'); // Show the alert
   info.querySelector('span').textContent = text;
   setTimeout(() => {
-  document.getElementById('toast').classList.add("hidden");
-}, 1000);
+    document.getElementById('toast').classList.add("hidden");
+  }, 1000);
 }
 function eventy() {
   navigator.clipboard.writeText(url)
-  .then(() => {
-    info("Addresse kopiert")
-  })
-  .catch(err => {
-    console.error("Failed to copy: ", err);
-  });
+    .then(() => {
+      info("Addresse kopiert")
+    })
+    .catch(err => {
+      console.error("Failed to copy: ", err);
+    });
 
 
 }
