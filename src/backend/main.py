8fe59918bccdadebe77045ba418c308bda6ae29c
JayPoch <technobladedeutsch@gmail.com>
2025-05-26 18:40:48 +0200

    cleanup

---
+++ src/backend/main.py
@@ -0,0 +1,44 @@
+import uuid
+from flask import Flask, jsonify, request
+from flask_cors import CORS # type: ignore
+from image import image_base, user
+app = Flask(__name__)
+_ = CORS(app)
+images = {"example": image_base("example", "	http://0.0.0.0:2020/images/866-536x354.jpg", {"fstop": True}, {
+    "camera_model": "Canon EOS R5",
+    "lens_used": "RF 50mm f/1.2L USM",
+    "shutter_speed": "1/250 sec",
+    "aperture": "f/1.8",
+    "ISO": 200,
+    "lighting": "Studio Lampe",
+    "focal_point": "Beispiel Daten",
+    "photo_genre": "Portrait"})}
+user1 = user(images)
+
+
+@app.route("/<id>")
+def hello_world(id):
+    global user1
+    image = {"location": user1.images[id].location, "info_there": user1.images[id].info_there, "info": user1.images[id].info}
+    return jsonify(image)
+
+
+@app.route("/add_image", methods=["POST"])
+def save_image():
+    global user1
+    data = request.get_json() 
+    image1 = image_base(str(uuid.uuid4()), data["image_address"], {}, {
+    "camera_model": data["Kameramodell"],
+    "lens_used": data["Verwendetes_Objektiv"],
+    "shutter_speed": data["Belichtungszeit"],
+    "aperture": data["Blende"],
+    "ISO": data["ISO"],
+    "lighting": data["Beleuchtung"],
+    "focal_point": data["Fokuspunkt"],
+    "photo_genre": data["Fotogenre"]})
+    user1.images[image1.identificator] = image1
+    return jsonify({"id": image1.identificator}), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True) 
---
+++ src/frontend/script.js
@@ -0,0 +1,60 @@
+const url = window.location.href;
+async function myFunction(id) {
+  try {
+    let response = await fetch('http://127.0.0.1:5000/' + id);
+    let data = await response.json();
+    console.log(data);
+    return data; // Returns fetched data
+  } catch (error) {
+  }
+}
+
+const urlParams = new URLSearchParams(window.location.search);
+const contentId = urlParams.get('content_id');
+
+async function run() {
+  let result = await myFunction(contentId);
+  //console.log(result)
+  document.getElementById('image').src = result.location;
+  document.getElementById('textbox2').textContent =
+    "Kameramodell: " + result.info.ISO
+    + "\nVerwendetes Objektiv: " + result.info.camera_model
+    + "\nBelichtungszeit: " + result.info.lens_used
+    + "\nBlende: " + result.info.shutter_speed
+    + "\nISO: " + result.info.aperture
+    + "\nBeleuchtung: " + result.info.lighting
+    + "\nFokuspunkt: " + result.info.focal_point
+    + "\nFotogenre: " + result.info.photo_genre
+}
+run();
+function info(text) {
+  const info = document.getElementById('info');
+  document.getElementById('toast').classList.remove('hidden'); // Show the alert
+  info.querySelector('span').textContent = text;
+  setTimeout(() => {
+    document.getElementById('toast').classList.add("hidden");
+  }, 1000);
+}
+function eventy() {
+  navigator.clipboard.writeText(url)
+    .then(() => {
+      info("Addresse kopiert")
+    })
+    .catch(err => {
+      console.error("Failed to copy: ", err);
+    });
+
+
+}
+document.getElementById("share").addEventListener("click", eventy)
+
+
+
+
+
+
+
+
+
+
+
